1b00e64d2aea959c1a6256e092e7f174f75214a8
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2019-12-01 11:22:22 +0100

    Solved day one

--- 1/main.py
+++ 1/main.py
@@ -8,10 +8,30 @@ def readInput():
     lines = file.readlines()
     return lines
 
+def calcFuel( amount ):
+    return int(amount / 3) - 2
+
+def partOne( lines ):
+    sum = 0
+    for line in lines:
+        sum += calcFuel( int( line ) )
+    return sum
+
+def partTwo( lines ):
+    sum = 0
+    for line in lines:
+        num = calcFuel( int( line ) )
+        sum += num
+        while(num > 0):
+            num = calcFuel( int( num ) )
+            if( num > 0 ):
+                sum += num
+    return sum
+
 def main():
     lines = readInput()
-    print(lines)
-
+    print( "Part one: " + str( partOne( lines ) ) )
+    print( "Part two: " + str( partTwo( lines ) ) )
 
 if __name__ == "__main__":
     main()
