e6085a6accccb390dbbc86589f89005e96ef0038
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2019-12-02 09:09:03 +0100

    Finished part Two and prepared opcode part for other tasks

--- 2/main.py
+++ 2/main.py
@@ -8,9 +8,46 @@ def readInput():
     lines = file.readlines()
     return lines
 
+def partTwo( codes ):
+    memoryCopy = codes
+    for i in range(100):
+        for j in range(100):
+            codes = list( memoryCopy )
+            codes[1] = i
+            codes[2] = j
+            partOne(codes)
+            if( codes[0] == 19690720):
+                print(i , j)
+                return
+
+
+def partOne(codes):
+    instructionPointer = 0
+    while(codes[ instructionPointer ] != 99):
+        opCode = codes[ instructionPointer ]
+        if( opCode == 1 ):
+            pointerA = codes[ instructionPointer + 1 ]
+            pointerB = codes[ instructionPointer + 2 ]
+            pointerResult = codes[ instructionPointer + 3 ]
+            valueA = codes[ pointerA ]
+            valueB = codes[ pointerB ]
+            result = valueA + valueB
+            instructionPointer += 4
+            codes[ pointerResult ] = result
+        elif( opCode == 2 ):
+            pointerA = codes[ instructionPointer + 1 ]
+            pointerB = codes[ instructionPointer + 2 ]
+            pointerResult = codes[ instructionPointer + 3 ]
+            valueA = codes[ pointerA ]
+            valueB = codes[ pointerB ]
+            result = valueA * valueB
+            instructionPointer += 4
+            codes[ pointerResult ] = result
+
 def main():
     lines = readInput()
-    print(lines)
+    codes = list( map( lambda x: int(x), lines[0].split(',') ) )
+    partTwo( codes )
 
 
 if __name__ == "__main__":
