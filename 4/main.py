9e55e14d8daf4b2e95a167d9a8826793c5bee4f8
Sebastian Paulsen <sebastian_paulsen@paulsenkom.de>
2019-12-04 06:54:28 +0100

    Solution day 4

--- 4/main.py
+++ 4/main.py
@@ -8,9 +8,30 @@ def readInput():
     lines = file.readlines()
     return lines
 
+def partOne(min, max):
+    #bruteForce
+    counter = 0
+    for i in range(min, max + 1):
+        happy = True
+        double = False
+        tripple = False
+        for j in range(1, 6):
+            if( str(i)[j-1] > str(i)[j]):
+                happy = False
+            elif(str(i)[j-1] == str(i)[j]):
+                double = True
+                if(str(i).count(str(i)[j]) == 2):
+                    tripple = True
+
+        if(happy and double and tripple):
+            counter += 1
+            print(i)
+    print(counter)
+
 def main():
-    lines = readInput()
-    print(lines)
+    min = 234208
+    max = 765869
+    partOne(min, max)
 
 
 if __name__ == "__main__":
